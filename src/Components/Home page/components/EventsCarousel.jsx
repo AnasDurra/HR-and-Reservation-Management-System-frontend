202308cd854d59b414bb50d0c63d53e17b9c4efc
HadiBa707 <73488175+HadiBa707@users.noreply.github.com>
2023-08-22 21:23:46 +0300

    changed events card

--- src/Components/Home page/components/EventsCarousel.jsx
+++ src/Components/Home page/components/EventsCarousel.jsx
@@ -1,6 +1,7 @@
 import { ArrowUpOutlined, UserOutlined } from '@ant-design/icons';
 import { Card, Carousel, Col, Row, Statistic } from 'antd';
 import { useState } from 'react';
+import { IMAGE_URL } from '../../../redux/utils/constants';
 
 const carouselContentStyle = {
   height: '240px',
@@ -13,19 +14,38 @@ const carouselContentStyle = {
 function EventsCarousel({ events }) {
   const emptyEvents = [1, 2, 3];
 
+  const carouselContentStyle = {
+    position: 'absolute',
+    top: '75%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    color: 'white',
+    textAlign: 'center',
+    backgroundColor: 'rgba(12, 62, 237, 0.7)',
+    padding: '10px',
+    borderRadius: '12px'
+  };
+
   return (
     <Carousel autoplay={2}>
       {events?.length > 0
         ? events.map((event) => (
-            <div>
+          <div key={event.id}>
+            <div style={{ position: 'relative' }}>
+              <img
+                src={IMAGE_URL.concat(event.image)}
+                alt={event.title}
+                style={{ width: '100%', height: 'auto' }}
+              />
               <h3 style={carouselContentStyle}>{event.title}</h3>
             </div>
-          ))
-        : emptyEvents.map((event) => (
-            <div>
-              <h3 style={carouselContentStyle}>لا يوجد فعّاليات</h3>
-            </div>
-          ))}
+          </div>
+        ))
+        :
+        <div>
+          <h3 style={carouselContentStyle}>لا يوجد فعّاليات</h3>
+        </div>
+      }
     </Carousel>
   );
 }
